f24fc06ca41ed6754c426e401b8d1b74330c287b
ADMIN\Admin <tung.luu@savvycomsoftware.com>
2022-02-16 09:20:38 +0700

    commit validation form

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,397 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="content">
+    <h1 id="title" class="content--center">freeCodeCamp Survey Form</h1>
+    <p id="description" class="content--center">
+      Thank you for taking the time to help us improve the platform
+    </p>
+    <form action="#" id="survey-form" class="content__form">
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="name" id="name-label">Name:</label>
+        </div>
+        <div class="col-md-12">
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+            v-model="name"
+          />
+        </div>
+        <div
+          :class="{ active: isNameActive, 'text-danger': hasNameError }"
+        >Name must be filled out</div>
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="email" id="email-label">Email:</label>
+        </div>
+        <div class="col-md-12">
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            v-model="email"
+          />
+        </div>
+        <div
+          :class="{ active: isEmailActive, 'text-danger': hasEmailError }"
+        >{{emailErrorMessage}}</div>
+      </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="age" id="number-label">Age:</label>
+        </div>
+        <div class="col-md-12">
+          <input
+            type="number"
+            class="form-control"
+            id="age"
+            name="age"
+            min="9"
+            max="100"
+            placeholder="Age"
+            v-model="age"
+          />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="phone" id="phone-label">Phone:</label>
+        </div>
+        <div class="col-md-12">
+          <input
+            type="text"
+            class="form-control"
+            id="phone"
+            name="phone"
+            placeholder="Phone number"
+            v-model="phone"
+          />
+        </div>
+        <div
+          :class="{ active: isPhoneActive, 'text-danger': hasPhoneError }"
+        >{{phoneErrorMessage}}</div>
+      </div>
+
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="role"
+            >Which option best describes your current role?</label
+          >
+        </div>
+        <div class="col-md-12">
+          <select v-model="role" id="role" name="role" class="form-control">
+            <option disabled selected value>Select current role</option>
+            <option v-for="option in options" :value="option.value">
+              {{ option.text }}
+            </option>
+          </select>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="userRecommend"
+            >Would you recommend freeCodeCamp to a friend?</label
+          ><br />
+        </div>
+        <div class="col-md-12">
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="radio"
+                class="form-check-input"
+                value="definitely"
+                name="userRecommend"
+                v-model="userRecommend"
+              />Definitely
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="radio"
+                class="form-check-input"
+                value="maybe"
+                name="userRecommend"
+                checked
+                v-model="userRecommend"
+              />Maybe
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="radio"
+                class="form-check-input"
+                value="not-sure"
+                name="userRecommend"
+                v-model="userRecommend"
+              />Not sure
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col content__form__label">
+          <label for="prefer"
+            >What would you like to see improved? (Check all that apply)</label
+          >
+        </div>
+        <div class="col-md-12">
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="front-end-projects"
+                v-model="prefer"
+              />Front-end Projects
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="back-end-projects"
+                v-model="prefer"
+              />Back-end Projects
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="data-visualization"
+                v-model="prefer"
+              />Data Visualization
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="challenges"
+                v-model="prefer"
+              />Challenges
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="open-source-community"
+                v-model="prefer"
+              />Open Source Community
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="gitter-help-rooms"
+                v-model="prefer"
+              />Gitter help rooms
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="videos"
+                v-model="prefer"
+              />Videos
+            </label>
+          </div>
+          <div class="form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                value="forum"
+                v-model="prefer"
+              />Forum
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col content__form__label">Any comments or suggestions?</div>
+        <div class="col-md-12">
+          <textarea
+            class="form-control"
+            rows="6"
+            cols="5"
+            id="comment"
+            v-model="comment"
+          ></textarea>
+        </div>
+      </div>
+    </form>
+    <div class="row">
+      <div class="col content__form__button">
+        <button class="btn btn-success" id="submit" v-on:click="submit()">
+          Submit
+        </button>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      age: 0,
+      phone: "",
+      role: "",
+      userRecommend: "",
+      prefer: [],
+      comment: "",
+      options: [
+        { text: "Student", value: "student" },
+        { text: "Full Time Job", value: "full-time-job" },
+        { text: "Full Time Learner", value: "full-time-learner" },
+        { text: "Prefer not to say", value: "prefer-not-to-say" },
+        { text: "Other", value: "other" },
+      ],
+      mailformat:  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      vnf_regex: /((09|03|07|08|05)+([0-9]{8})\b)/g,
+      hasNameError: false,
+      isNameActive: true,
+      hasEmailError: false,
+      isEmailActive: true,
+      hasPhoneError: false,
+      isPhoneActive: true,
+      nameErrorMessage:"",
+      emailErrorMessage:"",
+      phoneErrorMessage:"",
 
+    };
+  },
+  mounted() {
+    console.log(`the component is now mounted.`);
+  },
+  watch: {
+     name(value){
+      this.name = value;
+       if (this.name == "") {
+        this.hasNameError = true;
+        this.isNameActive = false;
+        this.nameErrorMessage = "Name must be filled out";
+      }
+      else {
+        this.hasNameError = false;
+        this.isNameActive = true;
+      } 
+    },
+
+     email(value){
+      this.email = value;
+      if (this.email != "") {
+        this.hasEmailError = false;
+        this.isEmailActive = true;
+         if (!this.email.match(this.mailformat)) {
+          this.hasEmailError = true;
+          this.isEmailActive = false;
+          this.emailErrorMessage = "You have entered an invalid email address!";
+        }
+      } else {
+          this.hasEmailError = true;
+          this.isEmailActive = false;
+          this.emailErrorMessage = "Email must be filled out!";
+      }
+    },
+
+     phone(value){
+      this.phone = value;
+      if (this.phone !== "") {
+          this.hasPhoneError = false;
+          this.isPhoneActive = true;
+          if (this.vnf_regex.test(this.phone) == false) {
+            this.hasPhoneError = true;
+            this.isPhoneActive = false;
+            this.phoneErrorMessage = "Invalid phone number!";
+          }
+      } else {
+          this.hasPhoneError = true;
+          this.isPhoneActive = false;
+          this.phoneErrorMessage = "Phone must be filled out!";
+      }
+    }
+  },
+  computed: {},
+  methods: {
+    submit() {
+      console.log(
+        this.name,
+        this.email,
+        this.age,
+        this.phone,
+        this.role,
+        this.userRecommend,
+        this.comment
+      );
+      this.prefer.forEach((element) => {
+        console.log(element);
+      });
+    },
+  },
+};
+</script>
 <style>
-@import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+@import "@/assets/base.css";
+body {
+  background-color: rgb(254, 236, 225);
+}
+.active{
+  display: none;
+}
+.content {
+  margin: 20px;
+  font-family: Tahoma, Verdana, sans-serif;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.content--center {
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.content__form {
+  width: 800px;
+  margin: 0px auto;
+  background-color: white;
+  border-radius: 4%;
+  padding: 30px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.content__form__label {
+  padding-top: 10px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.content__form__button {
+  text-align: center;
+  padding-top: 10px;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div style="display: flex">
+
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+  <label for="jack">Jack</label>
+
+  <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+  <label for="john">John</label>
+
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+  <label for="mike">Mike</label>
+</div>
 </template>
 
+<script lang="ts">
+export default {
+  data() {
+    return {
+      checkedNames: [],
+    };
+  },
+  computed: {},
+};
+</script>
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.greeting {
+  color: red;
+  font-weight: bold;
 }
 </style>
